eb12f82e1dfb17823c64e3c72f7c5a4662c4fd51
triphouse40 <125436384+triphouse40@users.noreply.github.com>
2025-04-03 16:46:30 +0200

    Non-functioning Login Page

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import REACT from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const AppFunction = () => {
+
+  return(
+    <SafeAreaProvider style={{backgroundColor: '#7F7F7F',}}>
+      <SafeAreaView>
+        <Text style={styles.con1}> Login</Text>
+        <Text style={styles.con2}> 
+          Username {'\n'}{'\n'}
+          <TextInput style={{borderWidth: 1, width: '100%', autofocus:true}}></TextInput>
+          {'\n'}{'\n'}{'\n'}{'\n'}
+          Password{'\n'}{'\n'}
+          <TextInput style={{borderWidth: 1, width: '100%',}}></TextInput>
+        </Text>
+      </SafeAreaView>
+    </SafeAreaProvider>   
   );
+
+};
+
+const styles = {
+  con1 : { 
+    padding: 50,
+    borderColor: 'black',
+    // borderWidth: 5,
+    textAlign: 'center',
+    marginTop: '20%',
+  },
+
+  con2 : {
+    padding: 50, 
+    borderColor: 'black', 
+    // borderWidth: 5, 
+    margin: 5,
+    borderRadius: 10,
+
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AppFunction;
